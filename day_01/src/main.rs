148037d3e908bdbacea247df4d5ef7805494385e
Ondřej Divina <ondrej.divina@kiwi.com>
2023-08-06 11:53:18 +0200

    feat: adapt for part 2 of day01

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,18 +1,40 @@
 use std::fs::read_to_string;
 
+fn read_file(path: &str) -> Vec<String> {
+    read_to_string(path).unwrap().lines().map(|s| s.to_string().clone()).collect()
+}
 
-fn main() {
-    let mut max_calories = 0;
-    let mut elf_calories = 0;
-    for line in read_to_string("src/input.txt").unwrap().lines(){
+fn get_elven_loads(lines: &Vec<String>) -> Vec<u32> {
+    let mut elves: Vec<Vec<u32>> = Vec::new();
+    let mut elf: Vec<u32> = Vec::new();
+    for line in lines {
         if line == "" {
-            if elf_calories > max_calories {
-                max_calories = elf_calories;
-            }
-            elf_calories = 0;
+            elves.push(elf);
+            elf = Vec::new();
             continue;
         }
-        elf_calories += line.to_string().parse::<u32>().unwrap()
+        elf.push(line.to_string().parse::<u32>().unwrap());
     }
+    let summed = elves.iter().map(|vec| vec.iter().sum()).collect::<Vec<u32>>();
+    summed
+}
+
+fn cound_sum_of_max_n(elves: &Vec<u32>, n: usize)  -> u32 {
+    let mut top_n_elves: Vec<&u32> = elves[0..n].iter().collect();
+    let mut nth_max = *top_n_elves.iter().min().expect("min not found");
+    for line in elves[n..].iter() {
+        if line > nth_max {
+            top_n_elves.swap_remove(top_n_elves.iter().position(|&x| x == nth_max).unwrap());
+            top_n_elves.push(line);
+            nth_max = *top_n_elves.iter().min().expect("min not found");
+        }
+    }
+    return top_n_elves.iter().map(|&e| e).sum();
+}
+
+fn main() {
+    let lines = read_file("src/input.txt");
+    let elves: Vec<u32> = get_elven_loads(&lines);
+    let max_calories = cound_sum_of_max_n(&elves, 3);
     println!("{}", max_calories);
 }
